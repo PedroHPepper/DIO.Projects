73c25f7a25013a7b607f9a3407af9dfb2bb23ee0
Pedro Pimenta <pedropimenta@rocketmail.com>
2021-04-26 16:06:07 -0300

    add outras solucoes

---
+++ Desenvolvendo-algoritmos-com-C-Sharp/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            int timeHoursSpent = int.Parse(input[0]);
+
+            int vMedia = int.Parse(input[1]);
+
+
+            int distance = vMedia * timeHoursSpent;
+
+            double litros = (double)distance / 12;
+
+            Console.WriteLine(litros.ToString("N3"));
+        }
+    }
+}
