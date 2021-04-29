30f3194e2c8cece192d2fc5a93ce93c2b262fc42
Pedro Pimenta <pedropimenta@rocketmail.com>
2021-04-29 11:06:31 -0300

    adicionando desafios aritmeticos DIO

---
+++ DesafiosAritmeticos-C-Sharp/ComunicacaoEmPiralandia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ComunicacaoEmPiralandia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            //continue a solução
+            char[] arr = n.ToCharArray();
+
+            Array.Reverse(arr);
+            string v = new string(arr);
+
+            Console.WriteLine(v);
+        }
+    }
+}
